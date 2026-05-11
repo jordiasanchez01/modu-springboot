b61d574e1e30eb6c28c0dfe1ccc02effa3e91c06
kseniia <kseniia@rudo.es>
2026-05-11 15:00:25 +0200

    [MDU-40]: modify jackson configuration no exlude null fields from responses, add message property to ErrorType, refactorGlobalControllerAdvice, add constraints to requests parameters of api-first contracts

--- Modu/driving/rest-api/src/main/java/com/laberit/Modu/rest/advice/ErrorType.java
+++ Modu/driving/rest-api/src/main/java/com/laberit/Modu/rest/advice/ErrorType.java
@@ -1,9 +1,20 @@
 package com.laberit.Modu.rest.advice;
 
 public enum ErrorType {
-    VALIDATION_ERROR,
-    NOT_FOUND,
-    CONFLICT,
-    INTERNAL_ERROR
+    VALIDATION_ERROR ("Input validation failed. Check the fields for more details"),
+    NOT_FOUND(null),
+    CONFLICT(null),
+    INTERNAL_ERROR(null);
     //TODO: Add other types like Unauthorized, forbidden, if authorization will be finally required.
+
+    private final String message;
+
+
+    ErrorType(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
