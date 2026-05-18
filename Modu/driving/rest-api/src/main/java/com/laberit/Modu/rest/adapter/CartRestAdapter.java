8ff3b2ca6567eeb31656389b84eb16cba8bc96bc
kseniia <kseniia@rudo.es>
2026-05-18 15:20:42 +0200

    [MDU-12]: delete redundant boolean condition

--- Modu/driving/rest-api/src/main/java/com/laberit/Modu/rest/adapter/CartRestAdapter.java
+++ Modu/driving/rest-api/src/main/java/com/laberit/Modu/rest/adapter/CartRestAdapter.java
@@ -49,22 +49,18 @@ public class CartRestAdapter implements CartApi {
     @Override
     public ResponseEntity<CartResponse> updateCartItemQuantity(String xDeviceId, Long itemId, UpdateItemRequest updateItemRequest) {
         cartItemServicePort.updateCartItemQuantity(Long.valueOf(xDeviceId), itemId, cartItemMapper.toCommand(updateItemRequest));
-        boolean isPriceAlertNeeded = true;
-        return ResponseEntity.ok(buildCartResponse(xDeviceId, isPriceAlertNeeded));
+        return ResponseEntity.ok(buildCartResponse(xDeviceId));
     }
 
     @Override
     public ResponseEntity<CartResponse> deleteCartItem(String xDeviceId, Long itemId) {
         cartItemServicePort.deleteCartItemById(Long.valueOf(xDeviceId), itemId);
-        boolean isPriceAlertNeeded = true;
-        return ResponseEntity.ok(buildCartResponse(xDeviceId, isPriceAlertNeeded));
+        return ResponseEntity.ok(buildCartResponse(xDeviceId));
     }
 
-    private CartResponse buildCartResponse(String xDeviceId, boolean isPriceAlertNeeded) {
+    private CartResponse buildCartResponse(String xDeviceId) {
         GetCartResponse getCartResponse = cartServicePort.findCartByUserId(Long.valueOf(xDeviceId));
         CartResponse cartResponse = cartMapper.toCartResponse(getCartResponse.cart());
-        if (isPriceAlertNeeded) cartResponse.setPriceChangedAlert(
-                checkIfPricesChanged(getCartResponse.changedPrices()));
         return cartResponse;
     }
 
