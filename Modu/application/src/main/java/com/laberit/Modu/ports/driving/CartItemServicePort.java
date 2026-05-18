a65ff1dfadca5d0a360668bba982d621e6727bc0
kseniia <kseniia@rudo.es>
2026-05-18 12:03:20 +0200

    [MDU-11]: rename UpdateCartItemCommand to UpdateCartItemQuantityCommand

--- Modu/application/src/main/java/com/laberit/Modu/ports/driving/CartItemServicePort.java
+++ Modu/application/src/main/java/com/laberit/Modu/ports/driving/CartItemServicePort.java
@@ -1,9 +1,9 @@
 package com.laberit.Modu.ports.driving;
 
 import com.laberit.Modu.domain.model.CartItem;
-import com.laberit.Modu.ports.driving.command.UpdateCartItemCommand;
+import com.laberit.Modu.ports.driving.command.UpdateCartItemQuantityCommand;
 
 public interface CartItemServicePort {
 
-    CartItem updateCartItemQuantity(Long userId, Long cartItemId, UpdateCartItemCommand command);
+    CartItem updateCartItemQuantity(Long userId, Long cartItemId, UpdateCartItemQuantityCommand command);
 }
--- Modu/application/src/main/java/com/laberit/Modu/services/CartItemServiceUseCase.java
+++ Modu/application/src/main/java/com/laberit/Modu/services/CartItemServiceUseCase.java
@@ -7,7 +7,7 @@ import com.laberit.Modu.ports.driven.CartItemRepositoryPort;
 import com.laberit.Modu.ports.driven.CartRepositoryPort;
 import com.laberit.Modu.ports.driving.CartItemServicePort;
 import com.laberit.Modu.ports.driving.ProductVariantServicePort;
-import com.laberit.Modu.ports.driving.command.UpdateCartItemCommand;
+import com.laberit.Modu.ports.driving.command.UpdateCartItemQuantityCommand;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -23,7 +23,7 @@ public class CartItemServiceUseCase implements CartItemServicePort {
     private final CartRepositoryPort cartRepositoryPort;
 
     @Override
-    public CartItem updateCartItemQuantity(Long userId, Long cartItemId, UpdateCartItemCommand command) {
+    public CartItem updateCartItemQuantity(Long userId, Long cartItemId, UpdateCartItemQuantityCommand command) {
         int requestedQuantity = command.quantity();
         Cart cart = cartRepositoryPort.findByUserId(userId).orElseThrow(
                 () -> new CartNotFoundException(userId));
