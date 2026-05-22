1b5a5bde2df8f58fe82b6b2c801e36685dea31d9
RED\jasanchez <jordisanchez@rudo.es>
2026-05-22 11:12:45 +0200

    [MDU-X-Checkout-Refactor]: OrderServiceUseCase now checks in the getOrder method if the Cart it has received has cart items or is empty, and returns correct error response.

--- Modu/application/src/main/java/com/laberit/Modu/services/OrderServiceUseCase.java
+++ Modu/application/src/main/java/com/laberit/Modu/services/OrderServiceUseCase.java
@@ -1,5 +1,6 @@
 package com.laberit.Modu.services;
 
+import com.laberit.Modu.domain.exceptions.CartEmptyException;
 import com.laberit.Modu.domain.exceptions.OrderNotFoundException;
 import com.laberit.Modu.domain.exceptions.OrderNotPaidException;
 import com.laberit.Modu.domain.model.*;
@@ -53,6 +54,10 @@ public class OrderServiceUseCase implements OrderServicePort {
             cartServicePort.updateCart(command.cartToOrder());
 
             CartWithPriceAndStockCheck cartResponse = cartServicePort.getCartWithPriceAndStockCheck(deviceId);
+
+            if (cartResponse.cart().getCartItems().isEmpty()) {
+                throw new CartEmptyException(deviceId);
+            }
 
             if (cartResponse.changedPrices().isEmpty() && cartResponse.insufficientStock().isEmpty()) {
                 order.setDeviceId(deviceId);
