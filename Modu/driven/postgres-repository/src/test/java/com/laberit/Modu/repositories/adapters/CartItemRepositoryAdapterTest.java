de6a279abf1923e28c4ec84c80b71b8af3a47354
RED\jasanchez <jordisanchez@rudo.es>
2026-06-01 15:27:45 +0200

    [MDU-Testing-01]: - CartItemRepositoryAdapterTest continued, more methods added.

--- Modu/driven/postgres-repository/src/test/java/com/laberit/Modu/repositories/adapters/CartItemRepositoryAdapterTest.java
+++ Modu/driven/postgres-repository/src/test/java/com/laberit/Modu/repositories/adapters/CartItemRepositoryAdapterTest.java
@@ -1,6 +1,8 @@
 package com.laberit.Modu.repositories.adapters;
 
+import com.laberit.Modu.domain.model.Cart;
 import com.laberit.Modu.domain.model.CartItem;
+import com.laberit.Modu.domain.model.ProductVariant;
 import com.laberit.Modu.repositories.CartItemJpaRepository;
 import com.laberit.Modu.repositories.mappers.CartItemPersistanceMapper;
 import com.laberit.Modu.repositories.models.CartItemEntity;
@@ -75,16 +77,120 @@ class CartItemRepositoryAdapterTest {
         }
     }
 
-    @Test
-    void findByProductVariantId() {
+    @Nested
+    @DisplayName("findByIdAndCartId() tests")
+    class FindByIdAndCartIdTests {
+        private Long id;
+        private CartItemEntity mockEntity;
+        private CartItem mappedCartItem;
+        private Cart mockCart;
+
+        @BeforeEach
+        void setUp() {
+            id = 1L;
+            mockEntity = new CartItemEntity();
+            mockEntity.setId(id);
+            mockCart = Cart.builder()
+                    .id(1L)
+                    .build();
+            mappedCartItem = CartItem.builder()
+                    .id(id)
+                    .cartId(mockCart.getId())
+                    .productVariantId(20L)
+                    .unitPrice(29.99)
+                    .quantity(2)
+                    .build();
+
+        }
+
+        @Test
+        @DisplayName("Returns the mapped CartItem wrapped in Optional when entity exists, and references correct CartId")
+        void findById_returnsMappedCartItem_whenEntityExistsAndReferencesCorrectCartId() {
+            when(cartItemJpaRepository.findByIdAndCartId(id, mockCart.getId())).thenReturn(Optional.of(mockEntity));
+            when(cartItemMapper.toDomain(mockEntity)).thenReturn(mappedCartItem);
+
+            Optional<CartItem> result = cartItemRepositoryAdapter.findByIdAndCartId(id, mockCart.getId());
+
+            assertTrue(result.isPresent());
+            assertEquals(mappedCartItem, result.get());
+            verify(cartItemJpaRepository).findByIdAndCartId(id, mockCart.getId());
+            verify(cartItemMapper).toDomain(mockEntity);
+        }
+
+        @Test
+        @DisplayName("Returns empty Optional when entity not found")
+        void findById_returnsEmpty_whenEntityNotFound() {
+            when(cartItemJpaRepository.findByIdAndCartId(id, mockCart.getId())).thenReturn(Optional.empty());
+
+            Optional<CartItem> result = cartItemRepositoryAdapter.findByIdAndCartId(id, mockCart.getId());
+
+            assertTrue(result.isEmpty());
+            verify(cartItemJpaRepository).findByIdAndCartId(id, mockCart.getId());
+            verifyNoInteractions(cartItemMapper);
+        }
     }
 
-    @Test
-    void findByIdAndCartId() {
-    }
+    @Nested
+    @DisplayName("findByCartIdAndProductVariantId() tests")
+    class FindByCartIdAndProductVariantIdTests {
+        private Long id;
+        private CartItemEntity mockEntity;
+        private CartItem mappedCartItem;
+        private Cart mockCart;
+        private ProductVariant mockProductVariant;
 
-    @Test
-    void findByCartIdAndProductVariantId() {
+        @BeforeEach
+        void setUp() {
+            id = 1L;
+            mockEntity = new CartItemEntity();
+            mockEntity.setId(id);
+            mockCart = Cart.builder()
+                    .id(1L)
+                    .build();
+            mockProductVariant = ProductVariant.builder()
+                    .id(20L)
+                    .build();
+            mappedCartItem = CartItem.builder()
+                    .id(id)
+                    .cartId(mockCart.getId())
+                    .productVariantId(mockProductVariant.getId())
+                    .unitPrice(29.99)
+                    .quantity(2)
+                    .build();
+
+        }
+
+        @Test
+        @DisplayName("Returns the mapped CartItem wrapped in Optional when entity exists, and references existing ProductVariantId")
+        void findById_returnsMappedCartItem_whenEntityExistsAndReferencesCorrectCartId() {
+            when(cartItemJpaRepository.findByCartIdAndProductVariantId(
+                    mockCart.getId(), mockProductVariant.getId())).thenReturn(Optional.of(mockEntity));
+            when(cartItemMapper.toDomain(mockEntity)).thenReturn(mappedCartItem);
+
+            Optional<CartItem> result = cartItemRepositoryAdapter.findByCartIdAndProductVariantId(
+                    mockCart.getId(), mockProductVariant.getId());
+
+            assertTrue(result.isPresent());
+            assertEquals(mappedCartItem, result.get());
+            verify(cartItemJpaRepository).findByCartIdAndProductVariantId(
+                    mockCart.getId(), mockProductVariant.getId());
+            verify(cartItemMapper).toDomain(mockEntity);
+        }
+
+        @Test
+        @DisplayName("Returns empty Optional when entity not found")
+        void findById_returnsEmpty_whenEntityNotFound() {
+            when(cartItemJpaRepository.findByCartIdAndProductVariantId(
+                    mockCart.getId(), mockProductVariant.getId())).thenReturn(Optional.empty());
+
+            Optional<CartItem> result = cartItemRepositoryAdapter.findByCartIdAndProductVariantId(
+                    mockCart.getId(), mockProductVariant.getId());
+
+            assertTrue(result.isEmpty());
+            verify(cartItemJpaRepository).findByCartIdAndProductVariantId(
+                    mockCart.getId(), mockProductVariant.getId());
+            verifyNoInteractions(cartItemMapper);
+        }
     }
 
     @Test
